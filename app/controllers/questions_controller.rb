44812d7fd73a1828de2d45d9bbf09a5cf986addf
tahia campos <tahia.campos@gmail.com>
2021-07-20 11:17:20 -0400

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,18 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
   def answer
     @answer = ""
     if params[:answer] == "I am going to work"
-      @answer = "Great!"
-    elsif params[:answer].last == "?"
-      @answer = "Silly question, get dressed and go to work!"
+      @answer = "-Coach: Great!"
+    elsif params[:answer].end_with?("?")
+      @answer = "-Coach: Silly question, get dressed and go to work!"
     else
-      @answer = "I don't care, get dressed and go to work!"
+      @answer = "-Coach: I don't care, get dressed and go to work!"
     end
+  end
+
+  def home
   end
 end
